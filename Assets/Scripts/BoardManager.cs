157620fb617fcd67996252b2603096567d17c151
Mike Lewis <mike_lewis_@hotmail.com>
2015-08-05 12:38:27 -0700

    added floor sprites

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+	public Transform FloorTop;
+	public GameObject FloorTopLeft;
+	public GameObject FloorTopRight;
+	public GameObject FloorMiddle;
+	public GameObject Floorleft;
+	public GameObject FloorRight;
+	public GameObject FloorBottom;
+	public GameObject FloorBottomRight;
+	public GameObject FloorBottomLeft;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
